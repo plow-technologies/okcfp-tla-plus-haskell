e7096652b6a3ef5f2dc867915a28287f68467cda
Scott Murphy <scott@ScottsM1Laptop.lan>
2022-08-02 21:39:02 -0500

    Finished writing the storage and network effects

--- src/System.hs
+++ src/System.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE DeriveGeneric #-}
 
@@ -15,34 +16,66 @@
 
 
 module System where
-
-import qualified Control.Monad.Reader 
-
+import qualified Data.Kind as Kind
+import qualified Control.Monad.Reader as Monad.Reader
+import Data.Functor.Identity(Identity)
 import GHC.Generics (Generic)
 
 
-data NetworkState = Active | Inactive 
+data TestNetworkState = Active | Inactive 
   deriving (Read,Eq,Show,Generic)
 
-data NetworkProfile = N1 | N2 
+data TestNetworkProfile = N1 | N2 
   deriving (Read,Eq,Show,Generic)
 
-data Status = Connected | Unconnected
+data TestStatus = Connected | Unconnected
   deriving (Read,Eq,Show,Generic)
 
 data Messages = None | Ping | Pong | Change 
+
+-----------------------------------------------------
+-- Envionrment and Monad Reader
+--------------------------------------------------
+type PingSyncM = Monad.Reader.ReaderT SystemEnv IO
+type PingSyncTestM = Monad.Reader.ReaderT SystemEnv Identity
+
+data SystemEnv = SystemEnv
+  { env :: ()}
 
 
 newtype Time = Time { unTime :: Int}
 
 
 -- | Our time type, usually UTCTime in prod and Int in testing
-class HasTick m where 
-  type Tick m 
-  getTick :: m 
+-- We are going to assume given states in our tla model of:
+-- NetworkState, NetworkProfile, Status, Messages
+-- Are not available to the system in a pure way by default
+-- therefore effect interfaces must be created to gather these states
+-- time is another value that must be gathered by side effect.
 
-class PingSyncInterface m where
- 
+class PingSyncSharedTypes (m:: (Kind.Type -> Kind.Type )) where
+  type Tick m  :: Kind.Type 
+  type Status m
+  type NetworkState m
+  type NetworkProfile m 
+  type SystemState m 
+
+-- | in the live system these would be the effects gathered by network
+-- actions 
+class PingSyncSharedTypes m =>   PingSyncNetworkEff m where
+  getTick :: m  (Tick m)
+  getNetworkState :: m (NetworkState m)
+  getNetworkProfile :: m (NetworkProfile m)  
+  getStatus :: m (Status m)
+
+-- | In the live system these would be the effects gathered by persistence
+
+class PingSyncSharedTypes m => PingSyncStorageEff m where
+   getSystemState :: m (SystemState m)  
+   modifySystemState :: ((SystemState m) -> (SystemState m)) -> m (SystemState m)
+   putSystemState :: (SystemState m) -> m ()
+
+
 
 runSystem :: IO () 
 runSystem = putStrLn "System Started"
